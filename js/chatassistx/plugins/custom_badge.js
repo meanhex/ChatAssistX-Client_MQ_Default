2151bbd3b1ddc536a1ab3a233bfa7a7c1d96fdc4
meanhex <meanhex7@gmail.com>
2024-03-29 22:53:06 +0900

    Add files via upload

---
+++ js/chatassistx/plugins/custom_badge.js
@@ -0,0 +1,41 @@
+(function(window) {
+	var plugin_name = "custom_badge";
+
+	if (typeof window.ChatAssistX.plugins[plugin_name] !== 'undefined') {
+		console.log(plugin_name.capFirst() + " plugin is already loaded!");
+	} else {
+		console.log(plugin_name.capFirst() + " plugin is loaded");
+		window.ChatAssistX.plugins[plugin_name] = {};
+		window.ChatAssistX.plugins[plugin_name].process = function(args, config) {
+			var nickname = args.nickname;
+			if(typeof config === 'undefined') {
+				console.warn("custom_badge plugin loaded without custom_badge_list. quitting...");
+				window.ChatAssistX.plugins[plugin_name].process = function(args, config) {
+					return false;
+				};
+			}
+			
+			if(typeof config === 'string') {
+				var old_config;
+				config = [];
+				config.push(old_config);
+				
+				console.warn("custom_badge_list should be array, not string.");
+			}
+			
+			for(var findword in config.custom_badge_list) {
+				// 닉네임이 있을때
+				if (nickname === findword){
+					nickname = '<img style="vertical-align: middle; width: 18px;" src="' + config.custom_badge_list[findword] + '" alt="Broadcaster" class="badge">&nbsp;' + nickname;
+				}
+			}
+			
+			if(args.nickname != nickname) return nickname;
+			else return false;
+		};
+		
+		window.ChatAssistX.plugins[plugin_name].init = function(config) {
+			return true;
+		};
+	}
+})(window);
